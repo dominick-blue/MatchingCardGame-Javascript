cd08b248d2563a31c626d6b5a764cccce2ddd818
Dominick Blue <65993112+Dominick-Blue1@users.noreply.github.com>
2021-09-11 15:30:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 function appendNewCard(parentElement) {
-/***
-OVERVIEW:
-This is the first function we're going to write in our project. The purpose of this function is to add a single new card element to the page (it won't have any pictures yet). We are going to "construct" this element and place it on the page using our knowledge of DOM Manipulation.
-
-INPUT/OUTPUT: 
-The appendNewCard function takes in a parent HTML element as a parameter named 'parentElement'. The parent element will look like this:
-
-  <div id="card-container">
-  </div>
-
-After the function call the parentElement should look like this:
-
-  <div id="card-container">
-    <div class="card">
-      <div class="card-down"></div>
-      <div class="card-up"></div>
-    </div>
-  </div>
-
-To accomplish this, the function is should create the new card element (i.e. .card), append it as a child to the parentElement (i.e. #card-container), and return the new card element.
-*/
-
   // Step 1: Make a variable for the card element. Assign it to a new div element.
 
   var newCard = document.createElement("div");
@@ -31,36 +9,21 @@ To accomplish this, the function is should create the new card element (i.e. .ca
   newCard.innerHTML = '<div class="card-down"></div>' + '<div class="card-up"></div>';  
   
   // Step 4: Append the card element to the parentElement, making the card element a "child".
-
   parentElement.appendChild(newCard);
 
   // Step 5: Return the card element.
-
   return newCard;
-
 }
 
 // appendNewCardTest();
 
-/***  shuffleCardImageClasses()
-
-OVERVIEW:
-We've defined image classes in the CSS named 'image-1' through 'image-6' that, when applied to a card, will make it show that particular image when it's flipped. Since the matching game works with pairs of images, we want to generate a random array with two of each image class string (12 total).
-
-INPUT/OUTPUT: 
-Returns an array with 12 randomly ordered image classes (i.e. image-X, where X is a value between 1 and 6). There should be exactly 2 of each image class in the array.
-*/
 function shuffleCardImageClasses() {
   
   // Step 1: Initialize an array of 2 of each image class strings in-order (e.g. "image-1", "image-1", "image-2"...)
   let randomImage = ["image-1", "image-1", "image-2", "image-2", "image-3", "image-3", "image-4", "image-4", "image-5", "image-5", "image-6", "image-6"];
-
-
+  
   randomImage = _.shuffle(randomImage);
-
-  /* Step 2: We're going to use a library to randomly "shuffle" the array we created. The library is called "underscore.js" because it uses an "_" charector as an object to contain helper methods.  Load underscore.js in your HTML via the CDN and then look at the "shuffle" method.  Note to ignore the "require" syntax as this is for non-browser environments (i.e. the var "_" will already be available to you from loading the CDN).
-  
-  
+  /* Step 2: We're going to use a library to randomly "shuffle" the array we created. The library is called "underscore.js" because it uses an "_" charector as an object to contain helper methods.  Load underscore.js in your HTML via the CDN and then look at the "shuffle" method.  Note to ignore the "require" syntax as this is for non-browser environments (i.e. the var "_" will already be available to you from loading the CDN). 
 
   CDN: https://cdnjs.com/libraries/underscore.js/1.4.1
    
@@ -68,33 +31,15 @@ function shuffleCardImageClasses() {
    */
    
   // Step 3: Return the shuffled array of class names.
-
   return randomImage;
-
 }
 
-
-// shuffleCardImageClassesTest();
-/***  createCards()
-
-OVERVIEW:
-For each of the 12 cards in the game, this function will create a card, assign it a random image class, and create an object to represent that card in our program.
-
-INPUT/OUTPUT:  
-The 'parentElement' parameter is the DOM element where the cards should be appended as children (i.e. #card-container). 
-
-The 'shuffledImageClasses' parameter is an array of 12 image class strings (e.g. "image-1", "image-5", "image-3"...) randomly ordered and with 2 strings from each image class.
-
-Returns an array of card objects to track all the cards for the rest of our program.
-*/
 function createCards(parentElement, shuffledImageClasses) {
   // Step 1: Make an empty array to hold our card objects.
   let cardHolder = [];
-  // Step 2: Loop 12 times to create the 12 cards we need.
-  
+  // Step 2: Loop 12 times to create the 12 cards we need.  
   for (let i = 0; i < shuffledImageClasses.length; i++) {
     
-  
     // Step 2(a): Use appendNewCard to create/append a new card and store the result in a variable.
     let addNewCard = appendNewCard(parentElement);
     // Step 2(b): Add an image class to the new card element, using shuffledImageClasses[i].
@@ -119,29 +64,14 @@ function createCards(parentElement, shuffledImageClasses) {
 // createCardsTest();
 
 
-/***  doCardsMatch
-
-OVERVIEW:
-Given two card objects, this will check if the card objects show the same image when flipped.
-
-INPUT/OUTPUT:  
-The 'cardObject1' parameter is the first card object in the comparison.
-
-The 'cardObject2' parameter is the second card object in the comparison.
-
-The function should return 'true' when both cards have the same imageClass property and 'false' otherwise.
-*/
 function doCardsMatch(cardObject1, cardObject2) {
     return cardObject1.imageClass === cardObject2.imageClass;
-
 }
 
 //doCardsMatchTest();
 
-
 /* An object used below as a dictionary to store counter names and their respective values.  Do you remember using objects as dictionaries? If not, go back to that lecture to review. */
 let counters = {};
-
 
 /***  incrementCounter 
 
